bc7847d203734ee198e36df20e3399a9bc743f49
rajaditya-m <rajadityamukherjee.osu@outlook.com>
2015-09-23 14:27:29 -0400

    All files

---
+++ src/basis_generation/double_domain_basis_generator.h
@@ -0,0 +1,42 @@
+#ifndef DOUBLEDOMAINBASISGENERATOR_H
+#define DOUBLEDOMAINBASISGENERATOR_H
+#include <vector>
+#include <functional>
+#include "subspace_tet.h"
+#include "single_domain_basis_generator.h"
+#include "basis_generator.h"
+
+
+class DoubleDomainBasisGenerator
+{
+public:
+  DoubleDomainBasisGenerator(const char *mesh_file_uncons, const char *mesh_file_cons, AffineTransformer<double>* transformer = NULL);
+  void preLoad(const char* prefix);
+  /*SingleDomainBasisGenerator(const char* mesh_file,
+                            AffineTransformer<double>* transformer = NULL);
+  void GenerateBasis(const char *basis_prefix, int linear_basis_num, int final_basis_num);
+  void RegenerateStitchBasis(int modeID);
+  void preLoad(const char* prefix);
+  void setStitchedStiffnessMatrix(SparseMatrix* spmat);
+  void SetFixedVertex(std::function<bool (int, double *)> IsFixed);
+  void SetFixedVertex(std::vector<int> fixed_verts);
+  void ProcessFixedVertex(const char *filename);
+
+  void setStiffyMult(int s) { basis_generator->setStiffyMultiplier(s);}
+
+
+
+  /// select the $fixed_vert_num_per_domain$ number vertex that are closest to
+  /// the center of mass for each partition as fixed vertices
+  void SetFixedVertex(int fixed_vert_num);
+  virtual ~SingleDomainBasisGenerator();
+  std::vector<int> fixed_verts_;
+  char file_name_[1024];
+
+  BasisGenerator *basis_generator;*/
+
+  SingleDomainBasisGenerator *constrained_portion_;
+  SingleDomainBasisGenerator *unconstrained_portion_;
+};
+
+#endif // SINGLEDOMAINBASISGENERATOR_H
